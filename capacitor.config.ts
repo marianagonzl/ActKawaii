01c74bf90ff52bf21327b314b2e3c4bbe9b00761
marianagonzl <10193@portalcedi.edu.mx>
2025-03-12 15:22:25 -0600

    primer commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ActKawaii',
+  webDir: 'www'
+};
+
+export default config;
